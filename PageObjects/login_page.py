009d72c51c741c52619ed5d266778d240e141251
congFly <1064607661@qq.com>
2020-03-02 22:44:43 +0800

    登录模块用例

--- PageObjects/login_page.py
+++ PageObjects/login_page.py
@@ -11,11 +11,19 @@ class LoginPage:
 
     # 登录功能
     def login(self, username, passwd):
-        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(By.XPATH("//input[@name='phone']")))
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//input[@name="phone"]')))
         self.driver.find_element_by_xpath('//input[@name="phone"]').send_keys(username)
         self.driver.find_element_by_xpath('//input[@name="password"]').send_keys(passwd)
         self.driver.find_element_by_xpath('//button').click()
 
-    # 获取错误提示-没有密码
-    def get_errorMsg_noPasswd(self):
-        pass
+    # 获取错误提示-没有密码-登录框区域提示
+    def get_errorMsg_from_loginArea(self):
+        WebDriverWait(self.driver, 20).until(
+            EC.visibility_of_element_located((By.XPATH, '//div[@class="form-error-info"]')))
+        return self.driver.find_element_by_xpath('//div[@class="form-error-info"]').text
+
+    # 获取错误提示-密码错误-页面中间提示
+    def get_errorMsg_from_pageCenter(self):
+        WebDriverWait(self.driver, 20).until(
+            EC.visibility_of_element_located((By.XPATH, '//div[@class ="layui-layer-content"]')))
+        return self.driver.find_element_by_xpath('//div[@class ="layui-layer-content"]').text
--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -9,6 +9,7 @@ class TestLogin(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.get('http://120.78.128.25:8765/Index/login.html')
         self.driver.maximize_window()
+
     def tearDown(self):
         self.driver.quit()
 
@@ -23,5 +24,21 @@ class TestLogin(unittest.TestCase):
     # 登录失败-没有密码
     def test_login_noPasswd(self):
         # 步骤：登录页面的登录功能+用户信息查询
+        lp = LoginPage(self.driver)
+        lp.login('18829291885', '')
         # 断言：错误提示信息是否正确
-        pass
+        self.assertEqual("请输入密码", lp.get_errorMsg_from_loginArea())
+
+    def test_login_noUser(self):
+        # 步骤：登录页面的登录功能+用户信息查询
+        lp = LoginPage(self.driver)
+        lp.login('', '123456fly')
+        # 断言：错误提示信息是否正确
+        self.assertEqual("请输入手机号", lp.get_errorMsg_from_loginArea())
+
+    def test_login_UserLess11(self):
+        # 步骤：登录页面的登录功能+用户信息查询
+        lp = LoginPage(self.driver)
+        lp.login('1882929188', '123456fly')
+        # 断言：错误提示信息是否正确
+        self.assertEqual("请输入正确的手机号", lp.get_errorMsg_from_loginArea())
