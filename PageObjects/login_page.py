b26c8489911170039fad3c26e2dd8b42fdcf5f9b
congFly <1064607661@qq.com>
2020-03-03 23:32:29 +0800

    提取元素定位到单个文件

--- PageObjects/login_page.py
+++ PageObjects/login_page.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+from PageLocators.loginPage_locator import LoginPageLocator as loc
 
 
 class LoginPage:
@@ -11,19 +12,19 @@ class LoginPage:
 
     # 登录功能
     def login(self, username, passwd):
-        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//input[@name="phone"]')))
-        self.driver.find_element_by_xpath('//input[@name="phone"]').send_keys(username)
-        self.driver.find_element_by_xpath('//input[@name="password"]').send_keys(passwd)
-        self.driver.find_element_by_xpath('//button').click()
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(loc.user_passwd))
+        self.driver.find_element(*loc.user_input).send_keys(username)
+        self.driver.find_element(*loc.user_passwd).send_keys(passwd)
+        self.driver.find_element(*loc.login_button).click()
 
     # 获取错误提示-没有密码-登录框区域提示
     def get_errorMsg_from_loginArea(self):
         WebDriverWait(self.driver, 20).until(
-            EC.visibility_of_element_located((By.XPATH, '//div[@class="form-error-info"]')))
-        return self.driver.find_element_by_xpath('//div[@class="form-error-info"]').text
+            EC.visibility_of_element_located(loc.errorMsg_from_loginArea))
+        return self.driver.find_element(*loc.errorMsg_from_loginArea).text
 
     # 获取错误提示-密码错误-页面中间提示
     def get_errorMsg_from_pageCenter(self):
         WebDriverWait(self.driver, 20).until(
-            EC.visibility_of_element_located((By.XPATH, '//div[@class ="layui-layer-content"]')))
-        return self.driver.find_element_by_xpath('//div[@class ="layui-layer-content"]').text
+            EC.visibility_of_element_located(loc.errorMsg_from_pageCenter))
+        return self.driver.find_element(*loc.errorMsg_from_pageCenter).text
