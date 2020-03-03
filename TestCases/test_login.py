03e204cddf40349257fd46f106eb794a553e196a
congFly <1064607661@qq.com>
2020-03-03 22:50:06 +0800

    使用setupClass修改为只打开一次浏览器，先执行错误的用例，再执行正确的用例

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -3,33 +3,46 @@ from PageObjects.login_page import LoginPage
 from PageObjects.index_page import IndexPage
 from selenium import webdriver
 import ddt
+from TestDatas import common_datas as CD
+from TestDatas import login_datas as TD
 
-datas = [{"user": "18829291885", "passwd": "", "check": "请输入密码"},
-         {"user": "", "passwd": "123456fly", "check": "请输入手机号"},
-         {"user": "1882929188", "passwd": "123456fly", "check": "请输入正确的手机号"}]
+
+# datas = [{"user": "18829291885", "passwd": "", "check": "请输入密码"},
+#          {"user": "", "passwd": "123456fly", "check": "请输入手机号"},
+#          {"user": "1882929188", "passwd": "123456fly", "check": "请输入正确的手机号"}]
 
 
 @ddt.ddt
 class TestLogin(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get('http://120.78.128.25:8765/Index/login.html')
-        self.driver.maximize_window()
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.get(CD.login_url)
+        cls.driver.maximize_window()
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+    # def setUp(self):
+    #     self.driver = webdriver.Chrome()
+    #     self.driver.get('http://120.78.128.25:8765/Index/login.html')
+    #     self.driver.maximize_window()
+    #
     def tearDown(self):
-        self.driver.quit()
+        self.driver.refresh()
 
     # 登录成功
-    def test_login_success(self):
+    def test_login_1_success(self):
         # 步骤：登录页面的登录功能+用户信息查询
-        LoginPage(self.driver).login('18829291885', '123456fly')
+        LoginPage(self.driver).login(TD.succ_data["user"], TD.succ_data["passwd"])
 
         # 断言：用户信息查询
         self.assertTrue(IndexPage(self.driver).isExit_quitEle())
 
     # 登录失败-没有密码
-    @ddt.data(*datas)
-    def test_login_wrongDatas(self, data):
+    @ddt.data(*TD.datas)
+    def test_login_0_wrongDatas(self, data):
         # 步骤：登录页面的登录功能+用户信息查询
         lp = LoginPage(self.driver)
         lp.login(data["user"], data["passwd"])
